2d48aff74a7c77a27720a747ae83612db7ce5946
Margherita <mtonon.ieu2023@student.ie.edu>
2025-06-21 15:28:06 +0200

    added siamese neural network information

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 
 #class RotationModel(nn.Module):
 #    def __init__(self, )
@@ -30,7 +31,32 @@ def build_custom_model(config):
     model = nn.Sequential(*layers) #creating the model
     return model
 
+#SIAMESE: we first define the Siamese model class
+class SiameseModel(nn.Module):
+    def __init__(self, embedding_net, comparison_net):
+        super().__init__()
+        self.embedding_net = embedding_net
+        self.comparison_net = comparison_net
+    
+    def forward(self, input1, input2):
+        emb1 = self.embedding_net(input1) 
+        emb2 = self.embedding_net(input2)
+        combined = torch.cat((emb1, emb2), dim=1) #combining the embeddings
+        output = self.comparison_net(combined)
+        return output.squeeze()
+
+def build_siamese_model(config):
+    """
+    Function to get the Siamese model for image rotation prediction.
+    Used in main.py
+    """
+    embedding_net = build_custom_model(config["model"]["embedding_layers"])
+    comparison_net = build_custom_model(config["model"]["comparison_layers"])
+    return SiameseModel(embedding_net, comparison_net)
+
 def get_model(config):
     if config["model"]["type"] == "custom":
         return build_custom_model(config)
+    elif config["model"]["type"] == "siamese":
+        return build_siamese_model(config)
     #can add more models here later - create functions that build the models based on different types/architectures
