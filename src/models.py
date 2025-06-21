ba7ab84d037736cef9007a388f8c306064f39592
Margherita <mtonon.ieu2023@student.ie.edu>
2025-06-21 11:46:05 +0200

    added a function that builds a NN based on custom architecture

--- src/models.py
+++ src/models.py
@@ -1,8 +1,35 @@
+import torch.nn as nn
+import torch
 
+#class RotationModel(nn.Module):
+#    def __init__(self, )
 
-def get_model():
+def build_custom_model(config):
     """
     Function to get the models for image rotation prediction.
     Used in main.py
     """
-    pass
+    layers = []
+    for layer in config["model"]["layers"]: #looping through the layers in the config
+        if "linear" in layer: #if the layer is a linear layer:
+            in_dim, out_dim = layer["linear"] #taking the dimensions of the layer
+            layers.append(nn.Linear(in_dim, out_dim))
+        elif "activation" in layer:
+            activation = layer["activation"]
+            if activation == "relu":
+                layers.append(nn.ReLU())
+            elif activation == "sigmoid":
+                layers.append(nn.Sigmoid())
+            elif activation == "tanh":
+                layers.append(nn.Tanh())
+            elif activation == "softmax":
+                layers.append(nn.Softmax(dim=1))
+        #can add more stuff here later if needed
+
+    model = nn.Sequential(*layers) #creating the model
+    return model
+
+def get_model(config):
+    if config["model"]["type"] == "custom":
+        return build_custom_model(config)
+    #can add more models here later - create functions that build the models based on different types/architectures
